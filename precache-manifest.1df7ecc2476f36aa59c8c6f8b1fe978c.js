1b8e6ec9acce9dce31b5c56fce11b8da05c872ce
varvind7 <varvind7@gmail.com>
2020-05-08 20:22:24 -0400

    Updates

---
+++ precache-manifest.1df7ecc2476f36aa59c8c6f8b1fe978c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7948f40079602fc0ad60c160f084753c",
+    "url": "/eportfolio/index.html"
+  },
+  {
+    "revision": "2d03d46e9f70c15c4a00",
+    "url": "/eportfolio/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "d55bb0eb3c9f5df670e4",
+    "url": "/eportfolio/static/css/main.aa71c7f1.chunk.css"
+  },
+  {
+    "revision": "2d03d46e9f70c15c4a00",
+    "url": "/eportfolio/static/js/2.2af03dfe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eportfolio/static/js/2.2af03dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55bb0eb3c9f5df670e4",
+    "url": "/eportfolio/static/js/main.6cf2d795.chunk.js"
+  },
+  {
+    "revision": "e2a1cbcc011000374c81",
+    "url": "/eportfolio/static/js/runtime-main.6606c507.js"
+  },
+  {
+    "revision": "5358eef07fb57f5f89aed2c796f357de",
+    "url": "/eportfolio/static/media/MyBurger.5358eef0.JPG"
+  },
+  {
+    "revision": "db0d31a2081198d8514830ab70f01f3d",
+    "url": "/eportfolio/static/media/antd.db0d31a2.jfif"
+  },
+  {
+    "revision": "0968ad8a7394cbda92da44db1563480b",
+    "url": "/eportfolio/static/media/coding.0968ad8a.jfif"
+  },
+  {
+    "revision": "2f3cc0aba2bf6af34bc43b483d06efb2",
+    "url": "/eportfolio/static/media/cpp.2f3cc0ab.jfif"
+  },
+  {
+    "revision": "c4f8de5e45dbcc43df46d6696390d002",
+    "url": "/eportfolio/static/media/github.c4f8de5e.png"
+  },
+  {
+    "revision": "50df3dacc9d36c38e90318469535ed20",
+    "url": "/eportfolio/static/media/hospital.50df3dac.JPG"
+  },
+  {
+    "revision": "40c8e61177633b621e98d4c757e0d21b",
+    "url": "/eportfolio/static/media/library.40c8e611.JPG"
+  },
+  {
+    "revision": "29fef91506c1750b02a2541b3f73f8d2",
+    "url": "/eportfolio/static/media/linkedin.29fef915.png"
+  },
+  {
+    "revision": "24bdba757dd086e5613bbae701151c8b",
+    "url": "/eportfolio/static/media/profile.24bdba75.jpg"
+  },
+  {
+    "revision": "aec696d08af0182c9da3fd3356609333",
+    "url": "/eportfolio/static/media/resume.aec696d0.pdf"
+  },
+  {
+    "revision": "4613a258609e4b1c8a1db7e4a385af64",
+    "url": "/eportfolio/static/media/vscode.4613a258.jfif"
+  },
+  {
+    "revision": "b8798e2b6062e1de60b0cdb1d897ef58",
+    "url": "/eportfolio/static/media/winhacks.b8798e2b.JPG"
+  }
+]);
